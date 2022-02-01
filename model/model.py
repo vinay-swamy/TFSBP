a07b7e31f3f21f17450085a2e140d5dfbd602a58
Vinay Swamy <vinayswamy@vinays-air.dyn.nyp.org>
2022-02-01 13:45:29 -0500

    change to proper convolutional model

--- model/model.py
+++ model/model.py
@@ -12,70 +12,55 @@ def lo(lin, padding, dilation, ks, stride):
 def get_conv_kernel_size(Li, Lo, padding, dilation):
     ks =((Lo-Li-2)*-1) +1
     return int(ks)
-
+#%%
 class BasicTFBSPredictor(nn.Module):
     def __init__(self, AA_mat_size, PWM_mat_size):
         super(BasicTFBSPredictor, self).__init__()
         self.PWM_mat_size = PWM_mat_size
         self.AA_mat_size = AA_mat_size
-
-        self.first_conv = nn.Sequential(
+        ## (batch, 20, AA_MAT_SIZE)
+        self.conv_block = nn.Sequential(
             nn.Conv1d(in_channels=20, 
-                      out_channels=8, 
-                      kernel_size = get_conv_kernel_size(AA_mat_size, AA_mat_size/2, 1, 1),
+                      out_channels=40, 
+                      kernel_size = 4,
+                      stride = 2,
                       padding=1
                       ),
             nn.ReLU(),
-            nn.Dropout(.3)
-        )   
-        self.second_conv =nn.Sequential(
-            nn.Conv1d(in_channels=8, 
-                      out_channels=4, 
-                      kernel_size= get_conv_kernel_size(AA_mat_size/2, AA_mat_size/4, 1, 1),
+            nn.Dropout(.3),
+            nn.MaxPool1d(2, 2),
+             ## (batch, 20, AA_MAT_SIZE/ 4)
+            nn.Conv1d(in_channels=40, 
+                      out_channels=80, 
+                      kernel_size= 4,
+                      stride = 2,
                       padding=1),
             nn.ReLU(),
-            nn.Dropout(.3)
+            nn.Dropout(.3),
+            nn.MaxPool1d(kernel_size = 2, stride = 2)
         )
-        self.last_conv = nn.Sequential(
-            nn.Conv1d(in_channels=4, 
-                      out_channels=2, 
-                      kernel_size= get_conv_kernel_size(AA_mat_size/4, AA_mat_size/8, 1, 1),
-                      padding=1),
+         ## (batch, 20, AA_MAT_SIZE/ 16)
+        self.dense_block =nn.Sequential(
+            nn.Linear(in_features=int(AA_mat_size/16 * 80 ),
+                     out_features=1024),
             nn.ReLU(),
-            nn.Dropout(.3)
-        )
-        self.first_dense =nn.Sequential(
-            nn.Linear(in_features=int(AA_mat_size/4 ),
-                     out_features=4*PWM_mat_size),
-            nn.ReLU(),
-            nn.Dropout(.3)
+            nn.Dropout(.3), 
+            nn.Linear(in_features=1024,
+                      out_features=256),
+                                  nn.ReLU(),
+            nn.Dropout(.3), 
+            nn.Linear(in_features=256, 
+                      out_features= 4 * self.PWM_mat_size)
+
         )
 
-        self.second_dense = nn.Sequential(
-            nn.Linear(in_features=4*PWM_mat_size, 
-                      out_features=4*PWM_mat_size),
-            nn.ReLU(),
-            nn.Dropout(.3)
-        )
-        self.third_dense = nn.Sequential(
-            nn.Linear(in_features=4*PWM_mat_size, 
-                      out_features=4*PWM_mat_size),
-            nn.ReLU(),
-            nn.Dropout(.3)
-        )
-        self.final_dense = nn.Linear(in_features=4*PWM_mat_size, out_features=4*PWM_mat_size)
-        self.sm = nn.Softmax(dim = 1)
+       
     def forward(self, x):
 
-        x=self.first_conv(x)        
-        x=self.second_conv(x)       
-        x=self.last_conv(x)       
+        x=self.conv_block(x)     
         x=torch.flatten(x, 1)       
-        x=self.first_dense(x)
-        x=self.second_dense(x)
-        x=self.third_dense(x)
-        x=self.final_dense(x)      
+        x=self.dense_block(x)
         x=torch.reshape(x, (-1,  4,self.PWM_mat_size))
-        x=self.sm(x)
         return x
 
+#%%
